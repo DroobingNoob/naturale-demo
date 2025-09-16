96ca7e18008a698bd2bf4c771e2cbb91a9b49b0c
aayushdasgupta <aayushdasgupta0408@gmail.com>
2025-09-16 06:22:03 +0530

    Fixed Full banner sizing on zoom out

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Banner = () => {
   return (
-    <section className="w-auto">
+    <section className="w-full">
       <img
         src="/banner-gpt.png"
         alt="Naturale Makhana Banner"
-        className="h-auto w-auto"
+        className="w-full h-auto max-w-screen-xl min-w-full mx-auto"
       />
     </section>
   );
